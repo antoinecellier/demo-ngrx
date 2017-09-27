34fbf22d78ee4a1aad18d6d5bb16a842fd7058c1
tbetous <tbetous@gmail.com>
2017-09-27 22:26:59 +0200

    Fix origin

--- src/app/tv.service.ts
+++ src/app/tv.service.ts
@@ -17,7 +17,8 @@ export class TVService {
       this.http.get(`https://api.themoviedb.org/3/tv/popular?api_key=${this.apiKey}`)
       .map(response => response.json())
       .map(json => json.results.splice(0, 10))
-      .map(series => search ? series.filter((serie: any) => serie.original_name.includes(search)) : series)                    
+      .map(series => search ? series.filter((serie: any) => serie.original_name.includes(search)) : series) 
+    return request(search)
   }
 
   // TODO: Function that return series as Observable
